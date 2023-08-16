2fdfea441c7b91a682f44fb18fa292382fcde16b
unknown <t.luiz@aluno.ifsp.edu.br>
2023-08-16 15:09:13 -0300

    Trabalho Ltp enviado

---
+++ src/main/java/br/edu/ifsp/wellbeing/wellbeingapi/WellbeingApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.wellbeing.wellbeingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/wellbeing/wellbeingapi/WellbeingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.wellbeing.wellbeingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
