be1f3f67ad6d6c317b719ad0d2e1df3b75f9a9be
dedthi <t.luiz@aluno.ifsp.edu.br>
2023-08-30 15:04:00 -0300

    Get e Post do Aluno

--- src/main/java/br/edu/ifsp/wellbeing/controller/AlunoController.java
+++ src/main/java/br/edu/ifsp/wellbeing/controller/AlunoController.java
@@ -1,18 +1,28 @@
 package br.edu.ifsp.wellbeing.controller;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifsp.wellbeing.Database;
 import br.edu.ifsp.wellbeing.model.Aluno;
+import br.edu.ifsp.wellbeing.model.AlunoDAO;
 
+@RestController
 public class AlunoController {
-	//https://localhost:8080/alunos
-    @GetMapping("/alunos")
-	public ArrayList<Aluno> recuperarAlunos(){
-		return Database.recupAlunos();
+	//thiago
+	//https://localhost:8080/listar/alunos
+    @GetMapping("/listar/alunos")
+	public List<Aluno> recuperarAlunos(){
+		AlunoDAO alunoDAO = AlunoDAO.getInstance();
+		return 	alunoDAO.read();
 	}
 
-	
+	@PostMapping("/cadastrar/aluno")
+	public void addAluno(@RequestBody Aluno novoAluno) {
+        Database.addDadoAluno(novoAluno);
+    }
 }
