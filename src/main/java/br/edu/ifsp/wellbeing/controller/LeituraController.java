749f9a2a4350dfd614944ca4f125afaeb7f49636
Pedro6Stein <65890570+Pedro6Stein@users.noreply.github.com>
2023-09-12 11:34:20 -0300

    LeituraGetAgoraVai
    parou de mostrar o monte de erro.

--- src/main/java/br/edu/ifsp/wellbeing/controller/LeituraController.java
+++ src/main/java/br/edu/ifsp/wellbeing/controller/LeituraController.java
@@ -1,7 +1,10 @@
 package br.edu.ifsp.wellbeing.controller;
 
+import java.sql.Date;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,57 +18,53 @@ import br.edu.ifsp.wellbeing.model.LeituraDAO;
 
 @RestController
 public class LeituraController {
-      //samuel
-     @GetMapping("wellbeing/leitura/{dia}")
-      public List<Leitura> consultLeitura(
-              @PathVariable("dia")
-              Integer dia
-      ){
-        System.out.println(dia);
-        List<Leitura> leituras = LeituraDAO.getInstance().getByDay(dia);
-         return leituras;
+  // samuel
+  @GetMapping("wellbeing/leitura/{dia}")
+  public List<Leitura> consultLeitura(
+      @PathVariable("dia") Integer dia) {
+    System.out.println(dia);
+    List<Leitura> leituras = LeituraDAO.getInstance().getByDay(dia);
+    return leituras;
+  }
+
+  // samuel
+  @PostMapping("wellbeing/leitura/cadastrar")
+  public void addLeitura(@RequestBody Leitura novaleitura) {
+    Database.addDadoLeitura(novaleitura);
+
+  }
+
+  // samuel
+  @DeleteMapping("wellbeing/leitura/deletar/{dia}")
+  public Leitura deleteLeitura(
+      @PathVariable("dia") Integer dia) {
+    List<Leitura> leituras = LeituraDAO.getInstance().getByDay(dia);
+    List<Leitura> listaLeituras = Database.recupLeituras();
+    for (Leitura leitura : listaLeituras) {
+      if (leitura.getDataEntrada().getDayOfMonth() == dia) {
+        Database.delDadoLeitura(leitura);
+        return leitura;
       }
-      //samuel
-      @PostMapping("wellbeing/leitura/cadastrar")
-      public void addLeitura(@RequestBody Leitura novaleitura){
-        Database.addDadoLeitura(novaleitura);
-        
-      }
-      //samuel
-      @DeleteMapping("wellbeing/leitura/deletar/{dia}")
-      public Leitura deleteLeitura(
-        @PathVariable("dia")
-              Integer dia
-        ){
-          List<Leitura> leituras = LeituraDAO.getInstance().getByDay(dia);
-          List<Leitura> listaLeituras = Database.recupLeituras();
-          for(Leitura leitura : listaLeituras){
-            if(leitura.getDataEntrada().getDayOfMonth() == dia){
-              Database.delDadoLeitura(leitura);
-              return leitura;
-            }
-          }
-          return null;
-        }
-      asdasdasd
+    }
+    return null;
+  }
 
-        //Stein
-        @GetMapping("/leitura/{dia}/{hora}/{aluno}")
-        public Leitura endPoint3(
-            @PathVariable("dia") Date dia,
-            @PathVariable("hora") Integer hora,
-            @PathVariable("aluno") String aluno) {
-            if (dia.before(new Date())) { 
-                throw new IllegalArgumentException("A data deve ser posterior à data atual");
-            }
-            if (aluno == null || aluno.isEmpty()) {
-                throw new IllegalArgumentException("O aluno deve ser especificado");
-            }
-            System.out.println(dia+" \\ "+hora+" \\ "+aluno);
-            Leitura leituras = LeituraDAO.getInstance().getByDay(dia).getByHour(hora).getByAluno(aluno);
-            return leituras; 
-        }
-      
-}asdas 
+  // Stein
+  
+@GetMapping("wellbeing/leitura/{dia}/{hora}")
+public ResponseEntity<List<Leitura>> consultLeitura(
+      @PathVariable("dia") Integer dia,
+      @PathVariable("hora") Integer hora) {
+    // Verifica se os parâmetros são válidos
+    if (dia <= 0 || hora <= 0) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+    // Obtém as leituras
+    List<Leitura> leituras = (List<Leitura>) ((LeituraDAO) LeituraDAO.getInstance().getByDay(dia)).getByHour(hora); //getByHour(hora) estava morte 
+                                                                                                                    //e meti umas correcao do propio sistema, 
+                                                                                                                    //o que resultou em "pegavel"
+    // Retorna a resposta
+    return ResponseEntity.ok(leituras);
+  }
 
-
+}
