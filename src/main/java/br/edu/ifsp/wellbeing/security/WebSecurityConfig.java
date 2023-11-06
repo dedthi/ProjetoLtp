d55ff0e0244616ed9e02928ebd4220bed45d1f59
dedthi <t.luiz@aluno.ifsp.edu.br>
2023-11-06 10:35:53 -0300

    verificar erro no requestMatchers (etapa 1)

--- src/main/java/br/edu/ifsp/wellbeing/security/WebSecurityConfig.java
+++ src/main/java/br/edu/ifsp/wellbeing/security/WebSecurityConfig.java
@@ -1,7 +1,41 @@
+package br.edu.ifsp.wellbeing.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
-
 public class WebSecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf().disable()
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(
+                authorize -> authorize.requestMatchers(HttpMethod.POST, "wellbeing/alunos/cadastrar").permitAll()
+                                      .requestMatchers(HttpMethod.GET, "wellbeing/alunos/listar").permitAll())
+            .authorizeHttpRequests(
+                authorize -> authorize.requestMatchers(HttpMethod.GET, "wellbeing/leitura/{id}").authenticated());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+        .username("ifsp")
+        .password("1234")
+        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
     
 }
